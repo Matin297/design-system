a6241cafe38812e915a8b93e81abd8ff3e57dc1e
Matin Mir <asmamir297@gmail.com>
2023-12-02 15:46:21 +0330

    avatar: add css for classes

---
+++ src/components/avatar/avatar.styles.ts
@@ -0,0 +1,44 @@
+import {css} from 'lit';
+
+export default css`
+  :host {
+    display: inline-block;
+
+    --size: 3rem;
+  }
+
+  .avatar {
+    width: var(--size);
+    height: var(--size);
+    font-size: calc(var(--size) * 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: silver;
+    overflow: hidden;
+  }
+
+  .avatar--circle {
+    border-radius: 50%;
+  }
+
+  .avatar--rounded {
+    border-radius: 4px;
+  }
+
+  .avatar--square {
+    border-radius: 0;
+  }
+
+  .avatar__icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .avatar__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
