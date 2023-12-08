6b61b418023a9cdc3cfd6ed59f7ca2a25b89ad7b
Matin Mir <asmamir297@gmail.com>
2023-12-08 11:17:46 +0330

    avatar: test image precedence over initials/icon

--- src/components/avatar/avatar.test.ts
+++ src/components/avatar/avatar.test.ts
@@ -18,7 +18,7 @@ describe('<ds-avatar>', () => {
     });
 
     it('should default to circle shape', () => {
-      const base = avatar.shadowRoot!.querySelector('[part="base"]')!;
+      const base = avatar.shadowRoot!.querySelector('[part=base]')!;
       expect(avatar).to.have.attribute('shape', 'circle');
       expect(base.className).to.include('avatar--circle');
     });
@@ -40,12 +40,12 @@ describe('<ds-avatar>', () => {
     });
 
     it('should render image part with the given image link as src', () => {
-      const imagePart = avatar.shadowRoot!.querySelector('[part="image"]');
+      const imagePart = avatar.shadowRoot!.querySelector('[part=image]');
       expect(imagePart).to.have.attribute('src', image);
     });
 
     it('should render the label attribute on the base part', () => {
-      const base = avatar.shadowRoot!.querySelector('[part="base"]');
+      const base = avatar.shadowRoot!.querySelector('[part=base]');
       expect(base).to.have.attribute('aria-label', label);
     });
   });
@@ -66,9 +66,46 @@ describe('<ds-avatar>', () => {
     });
 
     it('should render initials part with the given initials as text', () => {
-      const initialsPart =
-        avatar.shadowRoot!.querySelector('[part="initials"]');
+      const initialsPart = avatar.shadowRoot!.querySelector('[part=initials]');
       expect(initialsPart).to.have.text(initials);
+    });
+  });
+
+  describe('when image is provided, initials or icon should not render', () => {
+    const initials = 'SL';
+    const image =
+      'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+    const label = 'Small transparent square';
+
+    before(async () => {
+      avatar = await fixture<DsAvatar>(
+        html`
+          <ds-avatar
+            image=${image}
+            initials=${initials}
+            label=${label}
+          ></ds-avatar>
+        `
+      );
+    });
+
+    it('should be accessible', async () => {
+      await expect(avatar).to.be.accessible();
+    });
+
+    it('should render image part with src attribute of the given image link', () => {
+      const imagePart = avatar.shadowRoot!.querySelector('[part=image]');
+      expect(imagePart).to.have.attribute('src', image);
+    });
+
+    it('should not render initials part', () => {
+      const initialsPart = avatar.shadowRoot!.querySelector('[part=initials]');
+      expect(initialsPart).not.to.exist;
+    });
+
+    it('should not render icon', () => {
+      const iconSlot = avatar.shadowRoot!.querySelector('slot[name=icon]');
+      expect(iconSlot).not.to.exist;
     });
   });
 });
