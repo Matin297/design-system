8033768468c09dd6663a9c43a489c34825daecb3
Matin Mir <asmamir297@gmail.com>
2023-12-13 15:00:14 +0330

    internals: add event emitter to base element

---
+++ src/events/types.ts
@@ -0,0 +1,66 @@
+import type {WithRequiredProp} from '../utilities/types';
+
+type DsEventInit<T> = T extends keyof GlobalEventHandlersEventMap
+  ? GlobalEventHandlersEventMap[T] extends CustomEvent<Record<string, unknown>>
+    ? GlobalEventHandlersEventMap[T] extends CustomEvent<Record<string, never>>
+      ? CustomEventInit<GlobalEventHandlersEventMap[T]['detail']>
+      : Partial<
+          GlobalEventHandlersEventMap[T]
+        > extends GlobalEventHandlersEventMap[T]
+      ? CustomEventInit<GlobalEventHandlersEventMap[T]['detail']>
+      : WithRequiredProp<
+          CustomEventInit<GlobalEventHandlersEventMap[T]['detail']>,
+          'detail'
+        >
+    : CustomEventInit
+  : CustomEventInit;
+
+type DsCustomEvent<T> = T extends keyof GlobalEventHandlersEventMap
+  ? GlobalEventHandlersEventMap[T] extends CustomEvent<unknown>
+    ? GlobalEventHandlersEventMap[T]
+    : CustomEvent<unknown>
+  : CustomEvent<unknown>;
+
+type WithDetailsEvent<T> = T extends keyof GlobalEventHandlersEventMap
+  ? GlobalEventHandlersEventMap[T] extends CustomEvent<Record<string, unknown>>
+    ? GlobalEventHandlersEventMap[T] extends CustomEvent<Record<string, never>>
+      ? never
+      : Partial<
+          GlobalEventHandlersEventMap[T]
+        > extends GlobalEventHandlersEventMap[T]
+      ? never
+      : T
+    : never
+  : never;
+
+type WithDetailsEventsMap = {
+  [T in keyof GlobalEventHandlersEventMap as WithDetailsEvent<T>]: true;
+};
+
+type WithoutDetailsEvent<T> = T extends keyof GlobalEventHandlersEventMap
+  ? GlobalEventHandlersEventMap[T] extends CustomEvent<Record<string, unknown>>
+    ? GlobalEventHandlersEventMap[T] extends CustomEvent<Record<string, never>>
+      ? T
+      : Partial<
+          GlobalEventHandlersEventMap[T]
+        > extends GlobalEventHandlersEventMap[T]
+      ? T
+      : never
+    : T
+  : T;
+
+type WithoutDetailsEventsMap = {
+  [T in keyof GlobalEventHandlersEventMap as WithoutDetailsEvent<T>]: true;
+};
+
+type DsEventMap = WithDetailsEventsMap | WithoutDetailsEventsMap;
+
+export {
+  DsEventMap,
+  DsEventInit,
+  DsCustomEvent,
+  WithDetailsEvent,
+  WithDetailsEventsMap,
+  WithoutDetailsEvent,
+  WithoutDetailsEventsMap,
+};
--- src/internals/base-element.ts
+++ src/internals/base-element.ts
@@ -1,6 +1,16 @@
 import type {CSSResultGroup} from 'lit';
 import {LitElement, css} from 'lit';
 
+import type {
+  DsEventMap,
+  DsEventInit,
+  DsCustomEvent,
+  WithDetailsEvent,
+  WithDetailsEventsMap,
+  WithoutDetailsEvent,
+  WithoutDetailsEventsMap,
+} from '../events/types';
+
 export class BaseElement extends LitElement {
   static styles: CSSResultGroup = css`
     :host {
@@ -13,4 +23,29 @@ export class BaseElement extends LitElement {
       box-sizing: inherit;
     }
   `;
+
+  emit<T extends keyof WithDetailsEventsMap>(
+    name: WithDetailsEvent<T>,
+    options: DsEventInit<T>
+  ): DsCustomEvent<T>;
+  emit<T extends keyof WithoutDetailsEventsMap>(
+    name: WithoutDetailsEvent<T>,
+    options?: DsEventInit<T>
+  ): DsCustomEvent<T>;
+  emit<T extends keyof DsEventMap>(
+    name: T,
+    options?: DsEventInit<T>
+  ): DsCustomEvent<T> {
+    const customEvent = new CustomEvent(name, {
+      bubbles: true,
+      composed: true,
+      cancelable: false,
+      detail: {},
+      ...options,
+    });
+
+    this.dispatchEvent(customEvent);
+
+    return customEvent as DsCustomEvent<T>;
+  }
 }
