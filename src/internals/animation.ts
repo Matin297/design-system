69c51e005f13bd241c4a563e4e1848188e24ef9b
Matin Mir <asmamir297@gmail.com>
2023-12-13 14:40:05 +0330

    internals: add animation functionalities

---
+++ src/internals/animation.ts
@@ -0,0 +1,51 @@
+/*
+ * Stops all active animations on element.
+ * Tries to synchronize animation cancelation with browser rendering cycle.
+ * Returns a promise that will resolve after all animations are stopped.
+ */
+export function stopAnimations(element: HTMLElement) {
+  const animationPromises = element.getAnimations().map(
+    (animation) =>
+      new Promise((resolve) => {
+        requestAnimationFrame(resolve);
+        animation.cancel();
+      })
+  );
+
+  return Promise.all(animationPromises);
+}
+
+/*
+ * Animates an element using keyframes.
+ * Returns a promise that will resolve after the animation is finished or canceled.
+ */
+export function performAnimation(
+  element: HTMLElement,
+  keyframes: Keyframe[],
+  options?: KeyframeAnimationOptions
+) {
+  return new Promise((resolve) => {
+    const animation = element.animate(keyframes, {
+      ...options,
+      duration: prefersReducedMotion() ? 0 : options?.duration,
+    });
+
+    animation.addEventListener('finish', resolve, {once: true});
+    animation.addEventListener('cancel', resolve, {once: true});
+  });
+}
+
+export function shimKeyframesAutoHeight(
+  keyframes: Keyframe[],
+  calculatedValue: number
+) {
+  return keyframes.map(({height, ...props}) => ({
+    ...props,
+    height: height === 'auto' ? calculatedValue + 'px' : height,
+  }));
+}
+
+/** Tells if the user enables reduced motion setting in their browser or OS */
+function prefersReducedMotion() {
+  return window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+}
