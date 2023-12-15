54ddc2d4f3cf4894b5506ab3ca29d1d1fe4bc3cf
Matin Mir <asmamir297@gmail.com>
2023-12-15 14:17:19 +0330

    details: test hide/show states event being emitted

--- src/components/details/details.test.ts
+++ src/components/details/details.test.ts
@@ -1,6 +1,7 @@
 import './details.component.js';
 
-import {fixture, html, expect} from '@open-wc/testing';
+import {fixture, html, expect, oneEvent} from '@open-wc/testing';
+import sinon from 'sinon';
 import type DsDetails from './details.component';
 
 describe('<ds-details>', () => {
@@ -16,6 +17,21 @@ describe('<ds-details>', () => {
     it('should be accessible', async () => {
       await expect(details).to.be.accessible();
     });
+
+    it('should emit ds-hide and ds-hide-finish when calling hide()', async () => {
+      const hideHandler = sinon.spy();
+      const hideFinishHandler = sinon.spy();
+
+      details.addEventListener('ds-hide', hideHandler);
+      details.addEventListener('ds-hide-finish', hideFinishHandler);
+
+      details.hide();
+
+      await oneEvent(details, 'ds-hide-finish');
+
+      expect(hideHandler).to.have.been.calledOnce;
+      expect(hideFinishHandler).to.have.been.calledOnce;
+    });
   });
 
   describe('when details is closed', () => {
@@ -27,6 +43,21 @@ describe('<ds-details>', () => {
 
     it('should be accessible', async () => {
       await expect(details).to.be.accessible();
+    });
+
+    it('should emit ds-show and ds-show-finish when calling show()', async () => {
+      const showHandler = sinon.spy();
+      const showFinishHandler = sinon.spy();
+
+      details.addEventListener('ds-show', showHandler);
+      details.addEventListener('ds-show-finish', showFinishHandler);
+
+      details.show();
+
+      await oneEvent(details, 'ds-show-finish');
+
+      expect(showHandler).to.have.been.calledOnce;
+      expect(showFinishHandler).to.have.been.calledOnce;
     });
   });
 });
