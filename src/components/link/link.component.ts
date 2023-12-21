999cb07379a72de385a811184955937eb8a260c6
Matin Mir <asmamir297@gmail.com>
2023-12-21 11:00:50 +0330

    link: extend link from base button with its own props added

---
+++ src/components/link/link.component.ts
@@ -0,0 +1,48 @@
+import {customElement, property} from 'lit/decorators.js';
+import {literal, html} from 'lit/static-html.js';
+import {classMap} from 'lit/directives/class-map.js';
+import {ifDefined} from 'lit/directives/if-defined.js';
+import DsBaseButton from '../../internals/base-button/index.component';
+
+const ELEMENT_NAME = 'ds-link';
+
+@customElement(ELEMENT_NAME)
+export default class DsLink extends DsBaseButton {
+  tag = literal`a`;
+
+  /** The link address to attach to the anchor tag */
+  @property()
+  href = '';
+
+  /** The target to open the appointed link address into */
+  @property()
+  target: '_blank' | '_self' | '_parent' | '_top' = '_self';
+
+  /** The name as which to download the link address */
+  @property()
+  download?: string;
+
+  @property()
+  rel?: string;
+
+  render() {
+    return html`
+      <${this.tag}
+        part="base"
+        class=${classMap(this.classNames)}
+        href=${this.href}
+        target=${this.target}
+        rel=${ifDefined(this.rel)}
+        download=${ifDefined(this.download)}
+      >
+        ${this.slots}
+      </${this.tag}>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ELEMENT_NAME]: DsLink;
+  }
+}
