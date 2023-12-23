b305cca15b5caa3cd2686dfd055123fb94f0af7f
Matin Mir <asmamir297@gmail.com>
2023-12-23 16:41:54 +0330

    button: add tests when button takes part in submitting a form

--- src/components/button/button.test.ts
+++ src/components/button/button.test.ts
@@ -1,6 +1,7 @@
 import './button.component.js';
 
 import {fixture, html, expect} from '@open-wc/testing';
+import sinon from 'sinon';
 import DsButton from './button.component';
 
 describe('<ds-button>', () => {
@@ -39,6 +40,41 @@ describe('<ds-button>', () => {
     it('should disabled the underlying button element', () => {
       const base = button.shadowRoot!.querySelector('button[disabled]');
       expect(base).to.exist;
+    });
+  });
+
+  describe('when submitting a form', () => {
+    it('should submit the form when type is set to "submit" and placed inside of it', async () => {
+      const form = await fixture<HTMLFormElement>(html`
+        <form>
+          <ds-button type="submit">Submit</ds-button>
+        </form>
+      `);
+
+      const submitHandler = sinon.spy((e: Event) => e.preventDefault());
+      form.addEventListener('submit', submitHandler);
+
+      form.querySelector('ds-button')!.click();
+
+      expect(submitHandler).to.have.been.calledOnce;
+    });
+
+    it('should submit the form when type is set to "submit" and placed outside of it', async () => {
+      const form = await fixture<HTMLFormElement>(html`
+        <div>
+          <form id="testID">
+            <input name="test" value="test" />
+          </form>
+          <ds-button type="submit" form="testID">Submit</ds-button>
+        </div>
+      `);
+
+      const submitHandler = sinon.spy((e: Event) => e.preventDefault());
+      form.addEventListener('submit', submitHandler);
+
+      form.querySelector('ds-button')!.click();
+
+      expect(submitHandler).to.have.been.calledOnce;
     });
   });
 });
