ae4080ca2a98baf68a3d4926eddd7ea0d849e2bb
Matin Mir <asmamir297@gmail.com>
2023-12-28 15:39:51 +0330

    split-panel: add functionality for moving divider between panels

--- src/components/split-panel/split-panel.component.ts
+++ src/components/split-panel/split-panel.component.ts
@@ -1,9 +1,12 @@
-import {html} from 'lit';
+import {html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {BaseElement} from '../../internals/base-element';
+import {clamp, toPercent} from '../../utilities/math';
 import styles from './split-panel.styles';
 
 const ELEMENT_NAME = 'ds-split-panel';
+const DIVIDER_COLUM = 'var(--divider-width)';
+const END_COLUM = 'auto';
 
 @customElement(ELEMENT_NAME)
 export default class DsSplitPanel extends BaseElement {
@@ -11,11 +14,22 @@ export default class DsSplitPanel extends BaseElement {
 
   /** The current position of the divider from the start in percentage */
   @property({reflect: true, type: Number})
-  position = 60;
+  position = 50;
 
   /** Disables resizing through the divider */
   @property({type: Boolean, reflect: true})
   disabled = false;
+
+  willUpdate(changedProps: PropertyValues<this>) {
+    // Whenever position changes update the template columns on host
+    if (changedProps.has('position')) {
+      this.style.gridTemplateColumns = `
+        ${this._calcStartColumn()} 
+        ${DIVIDER_COLUM} 
+        ${END_COLUM}
+      `;
+    }
+  }
 
   render() {
     return html`
@@ -26,8 +40,44 @@ export default class DsSplitPanel extends BaseElement {
         role="separator"
         aria-valuenow=${this.position}
         tabindex=${this.disabled ? -1 : 0}
+        @pointerdown=${this._pointerDownHandler}
       ></div>
       <slot name="end-panel" part="end-panel" class="end-panel"></slot>
+    `;
+  }
+
+  private _pointerDownHandler() {
+    if (this.disabled) {
+      return;
+    }
+
+    // Read document-relative coordinates for host
+    const hostCoords = this.getCoords();
+
+    // Updates position with pointer's x coordinate
+    const pointerMoveHandler = (event: PointerEvent) => {
+      this.position = clamp(
+        0,
+        toPercent(event.pageX - hostCoords.left, this.offsetWidth),
+        100
+      );
+    };
+
+    const pointerUpHandler = () => {
+      document.removeEventListener('pointermove', pointerMoveHandler);
+    };
+
+    document.addEventListener('pointermove', pointerMoveHandler);
+    document.addEventListener('pointerup', pointerUpHandler, {once: true});
+  }
+
+  private _calcStartColumn() {
+    return `
+      clamp(
+        0%, 
+        calc(${this.position}% - ${DIVIDER_COLUM} / 2), 
+        calc(100% - ${DIVIDER_COLUM})
+      )
     `;
   }
 }
