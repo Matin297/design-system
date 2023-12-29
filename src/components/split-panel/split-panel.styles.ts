62878542682ed302c5f5a9645839970e5a92b45b
Matin Mir <asmamir297@gmail.com>
2023-12-29 11:24:18 +0330

    slipt-panel: support divider hit area for thin dividers

--- src/components/split-panel/split-panel.styles.ts
+++ src/components/split-panel/split-panel.styles.ts
@@ -3,7 +3,8 @@ import {css} from 'lit';
 export default css`
   :host {
     --min: 0%;
-    --divider-width: 10px;
+    --divider-width: 5px;
+    --divider-hit-area: 15px;
     --max: calc(100% - var(--divider-width));
 
     display: grid;
@@ -11,12 +12,17 @@ export default css`
   }
 
   .divider {
+    position: relative;
     display: flex;
     justify-content: center;
     align-items: center;
-    cursor: col-resize;
     color: var(--ds-color-neutral-900);
     background-color: var(--ds-color-neutral-200);
+  }
+
+  .divider::after {
+    content: '';
+    position: absolute;
   }
 
   .divider:focus {
@@ -27,13 +33,28 @@ export default css`
     background-color: var(--ds-color-primary-600);
   }
 
-  /** Disabled */
-  :host([disabled]) .divider {
-    cursor: not-allowed;
+  /** Horizontal */
+  :host(:not([vertical])) .divider {
+    cursor: col-resize;
+  }
+
+  :host(:not([vertical])) .divider::after {
+    height: 100%;
+    width: var(--divider-hit-area);
   }
 
   /** Vertical */
   :host([vertical]) .divider {
     cursor: row-resize;
+  }
+
+  :host([vertical]) .divider::after {
+    height: var(--divider-hit-area);
+    width: 100%;
+  }
+
+  /** Disabled */
+  :host([disabled]) .divider {
+    cursor: not-allowed;
   }
 `;
