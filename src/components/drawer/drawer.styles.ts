d97ce3aa25e0f7919746128f381fed05fbf67a72
Matin Mir <asmamir297@gmail.com>
2024-01-04 15:57:11 +0330

    drawer: improve drawer direction/placement styles

--- src/components/drawer/drawer.styles.ts
+++ src/components/drawer/drawer.styles.ts
@@ -16,19 +16,22 @@ export default css`
     border: none;
     max-height: 100%;
     max-width: 100%;
-
+    overflow: hidden;
     opacity: 0;
-    transition: opacity var(--ds-transition-medium) ease,
-      translate var(--ds-transition-medium) ease,
-      display var(--ds-transition-medium) ease allow-discrete,
-      overlay var(--ds-transition-medium) ease allow-discrete;
+
+    transition: opacity var(--ds-transition-medium) ease-in-out,
+      translate var(--ds-transition-medium) ease-in-out,
+      display var(--ds-transition-medium) ease-in-out allow-discrete,
+      overlay var(--ds-transition-medium) ease-in-out allow-discrete;
   }
 
   .drawer::backdrop {
     opacity: 0;
     background-color: hsl(240 3.8% 46.1% / 33%);
-    transition: opacity 250ms ease, display 250ms ease allow-discrete,
-      overlay 250ms ease allow-discrete;
+
+    transition: opacity 250ms ease-in-out,
+      display 250ms ease-in-out allow-discrete,
+      overlay 250ms ease-in-out allow-discrete;
   }
 
   .drawer[open],
@@ -36,19 +39,44 @@ export default css`
     opacity: 1;
   }
 
+  /** Body */
+  .drawer__body {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .drawer__header {
+    padding: var(--header-spacing);
+  }
+
+  .drawer__content {
+    flex-grow: 1;
+    overflow: auto;
+    padding: var(--content-spacing);
+  }
+
+  .drawer__footer {
+    padding: var(--footer-spacing);
+  }
+
   /** Inline */
   .drawer--inline {
     height: 100%;
     width: var(--size);
+    inset-block: 0;
   }
 
   .drawer--inline.drawer--end {
     translate: 100%;
+    inset-inline-end: 0;
     inset-inline-start: initial;
   }
 
   .drawer--inline.drawer--start {
     translate: -100%;
+    inset-inline-start: 0;
     inset-inline-end: initial;
   }
 
@@ -56,14 +84,18 @@ export default css`
   .drawer--block {
     width: 100%;
     height: var(--size);
+    inset-inline: 0;
   }
 
   .drawer--block.drawer--end {
     translate: 0 100%;
+    inset-block-end: 0;
     inset-block-start: initial;
   }
 
   .drawer--block.drawer--start {
+    translate: 0 -100%;
+    inset-block-start: 0;
     inset-block-end: initial;
   }
 
@@ -97,27 +129,5 @@ export default css`
     .drawer--block.drawer--start[open] {
       translate: 0 -100%;
     }
-  }
-
-  /** Body */
-  .drawer__body {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .drawer__header {
-    padding: var(--header-spacing);
-  }
-
-  .drawer__content {
-    flex-grow: 1;
-    overflow: auto;
-    padding: var(--content-spacing);
-  }
-
-  .drawer__footer {
-    padding: var(--footer-spacing);
   }
 `;
