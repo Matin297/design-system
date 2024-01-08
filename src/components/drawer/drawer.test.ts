54ba03928e5609cef443cb0cb9a23cd00d5a6752
Matin Mir <asmamir297@gmail.com>
2024-01-08 12:19:03 +0330

    drawer: test default state of the drawer

---
+++ src/components/drawer/drawer.test.ts
@@ -0,0 +1,20 @@
+import './drawer.component.js';
+
+import {fixture, html, expect} from '@open-wc/testing';
+import type DsDrawer from './drawer.component';
+
+describe('<ds-drawer>', () => {
+  describe('when no parameters are provided', () => {
+    it('should not show dialog by default', async () => {
+      const drawer = await fixture<DsDrawer>(
+        html`<ds-drawer>Here goes the content</ds-drawer>`
+      );
+
+      const dialog =
+        drawer.shadowRoot?.querySelector<HTMLDialogElement>('dialog');
+      const display = getComputedStyle(dialog!).display;
+
+      expect(display).to.be.equal('none');
+    });
+  });
+});
