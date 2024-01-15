adba397087cf8c8787cf98bca7c17ff50b5aec59
Matin Mir <asmamir297@gmail.com>
2024-01-15 16:48:29 +0330

    switch: add basic html structure

---
+++ src/components/switch/switch.component.ts
@@ -0,0 +1,31 @@
+import {html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+import {BaseElement} from '../../internals/base-element';
+
+const ELEMENT_NAME = 'ds-switch';
+
+@customElement(ELEMENT_NAME)
+export default class DsSwitch extends BaseElement {
+  static styles = [BaseElement.styles];
+
+  render() {
+    return html`
+      <label part="base" class="switch" for="switch">
+        <slot part="label" class="switch__label"></slot>
+        <input
+          id="switch"
+          part="input"
+          role="switch"
+          type="checkbox"
+          class="switch__input"
+        />
+      </label>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ELEMENT_NAME]: DsSwitch;
+  }
+}
