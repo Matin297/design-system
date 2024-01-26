6374528b18b19a1c3703939882b5d012f085e808
Matin Mir <asmamir297@gmail.com>
2024-01-26 12:05:03 +0330

    tab-group: refine stylings based on direction/placement

--- src/components/tab-group/tab-group.styles.ts
+++ src/components/tab-group/tab-group.styles.ts
@@ -6,7 +6,7 @@ export default css`
     --track-color: var(--ds-color-neutral-200);
     --active-color: var(--ds-color-primary-600);
 
-    --panel-spacing: var(--ds-spacing-medium) 0;
+    --group-spacing: var(--ds-spacing-medium);
     --tab-spacing: var(--ds-spacing-medium) var(--ds-spacing-large);
 
     display: block;
@@ -14,45 +14,91 @@ export default css`
 
   .tab-group {
     display: flex;
+    gap: var(--group-spacing);
   }
 
-  .tab-group__list {
+  .tab-group__tab-list {
+    display: flex;
     position: relative;
   }
 
   .tab-group__active-indicator {
     position: absolute;
-    inset-inline-start: 0;
-    height: var(--track-size);
     background-color: var(--active-color);
 
     transition: translate var(--ds-transition-fast) ease,
       width var(--ds-transition-fast) ease;
   }
 
-  /** Start */
-  .tab-group--start {
+  /** Inline */
+  :host([direction='inline']) .tab-group__tab-list {
+    flex-direction: row;
+  }
+
+  :host([direction='inline']) .tab-group__active-indicator {
+    inset-inline-start: 0;
+    height: var(--track-size);
+  }
+
+  /** Inline - Start */
+  :host([direction='inline']) .tab-group--start {
     flex-direction: column;
   }
 
-  .tab-group--start .tab-group__list {
-    border-bottom: var(--track-size) solid var(--track-color);
+  :host([direction='inline']) .tab-group--start .tab-group__tab-list {
+    border-block-end: var(--track-size) solid var(--track-color);
   }
 
-  .tab-group--start .tab-group__active-indicator {
+  :host([direction='inline']) .tab-group--start .tab-group__active-indicator {
     inset-block-start: 100%;
   }
 
-  /** End */
-  .tab-group--end {
+  /** Inline - End */
+  :host([direction='inline']) .tab-group--end {
     flex-direction: column-reverse;
   }
 
-  .tab-group--end .tab-group__list {
-    border-top: var(--track-size) solid var(--track-color);
+  :host([direction='inline']) .tab-group--end .tab-group__tab-list {
+    border-block-start: var(--track-size) solid var(--track-color);
   }
 
-  .tab-group--end .tab-group__active-indicator {
+  :host([direction='inline']) .tab-group--end .tab-group__active-indicator {
     inset-block-end: 100%;
+  }
+
+  /** Block */
+  :host([direction='block']) .tab-group__tab-list {
+    flex-direction: column;
+  }
+
+  :host([direction='block']) .tab-group__active-indicator {
+    inset-block-start: 0;
+    width: var(--track-size);
+  }
+
+  /** Block - Start */
+  :host([direction='block']) .tab-group--start {
+    flex-direction: row;
+  }
+
+  :host([direction='block']) .tab-group--start .tab-group__tab-list {
+    border-inline-end: var(--track-size) solid var(--track-color);
+  }
+
+  :host([direction='block']) .tab-group--start .tab-group__active-indicator {
+    inset-inline-start: 100%;
+  }
+
+  /** Block - End */
+  :host([direction='block']) .tab-group--end {
+    flex-direction: row-reverse;
+  }
+
+  :host([direction='block']) .tab-group--end .tab-group__tab-list {
+    border-inline-start: var(--track-size) solid var(--track-color);
+  }
+
+  :host([direction='block']) .tab-group--end .tab-group__active-indicator {
+    inset-inline-end: 100%;
   }
 `;
