a4bbcb2e6a8a3d0b156fa437f3687c48377695fe
Matin Mir <asmamir297@gmail.com>
2024-01-29 11:48:04 +0330

    tab-group: test not activating a disabled tab when clicked

--- src/components/tab-group/tab-group.test.ts
+++ src/components/tab-group/tab-group.test.ts
@@ -60,5 +60,29 @@ describe('<ds-tab-group>', () => {
       expect(tabs[0].active).to.be.false;
       expect(tabs[1].active).to.be.true;
     });
+
+    it('should not activate a disabled tab when clicked', async () => {
+      const tabGroup = await fixture<DsTabGroup>(html`
+        <ds-tab-group label="test">
+          <ds-tab slot="tabs" id="tab-1" panel="panel-1">tab</ds-tab>
+          <ds-tab-panel id="panel-1" tab="tab-1"> panel </ds-tab-panel>
+
+          <ds-tab slot="tabs" id="tab-2" panel="panel-2" disabled>tab</ds-tab>
+          <ds-tab-panel id="panel-2" tab="tab-2"> panel </ds-tab-panel>
+        </ds-tab-group>
+      `);
+
+      const tabs = tabGroup.querySelectorAll<DsTab>('ds-tab');
+
+      const {top, left} = tabs[1].getCoords();
+
+      await sendMouse({
+        type: 'click',
+        position: [Math.round(left + 1), Math.round(top + 1)],
+      });
+
+      expect(tabs[0].active).to.be.true;
+      expect(tabs[1].active).to.be.false;
+    });
   });
 });
