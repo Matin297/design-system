cf59719f4947528c1bd509417f8b58de19643819
Matin Mir <asmamir297@gmail.com>
2024-01-29 11:41:08 +0330

    tab-group: test tab activation upon clicking

--- src/components/tab-group/tab-group.test.ts
+++ src/components/tab-group/tab-group.test.ts
@@ -3,6 +3,7 @@ import './tab-panel.component.js';
 import './tab-group.component.js';
 
 import {fixture, html, expect} from '@open-wc/testing';
+import {sendMouse} from '@web/test-runner-commands';
 import type DsTabGroup from './tab-group.component';
 import type DsTab from './tab.component';
 
@@ -34,6 +35,30 @@ describe('<ds-tab-group>', () => {
 
       expect(tabs[0].active).to.be.true;
       expect(tabs[1].active).to.be.false;
+    });
+
+    it('should activate a tab by clicking on it and deactivate all others', async () => {
+      const tabGroup = await fixture<DsTabGroup>(html`
+        <ds-tab-group label="test">
+          <ds-tab slot="tabs" id="tab-1" panel="panel-1">tab</ds-tab>
+          <ds-tab-panel id="panel-1" tab="tab-1"> panel </ds-tab-panel>
+
+          <ds-tab slot="tabs" id="tab-2" panel="panel-2">tab</ds-tab>
+          <ds-tab-panel id="panel-2" tab="tab-2"> panel </ds-tab-panel>
+        </ds-tab-group>
+      `);
+
+      const tabs = tabGroup.querySelectorAll<DsTab>('ds-tab');
+
+      const {top, left} = tabs[1].getCoords();
+
+      await sendMouse({
+        type: 'click',
+        position: [Math.round(left + 1), Math.round(top + 1)],
+      });
+
+      expect(tabs[0].active).to.be.false;
+      expect(tabs[1].active).to.be.true;
     });
   });
 });
