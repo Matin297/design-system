be17fc8d057698b8ca47a202f2492b3bd72b86d3
Matin Mir <asmamir297@gmail.com>
2024-01-29 11:17:23 +0330

    tab-group: test default tab activation

--- src/components/tab-group/tab-group.test.ts
+++ src/components/tab-group/tab-group.test.ts
@@ -4,6 +4,7 @@ import './tab-group.component.js';
 
 import {fixture, html, expect} from '@open-wc/testing';
 import type DsTabGroup from './tab-group.component';
+import type DsTab from './tab.component';
 
 describe('<ds-tab-group>', () => {
   describe('when provided with no parameters', () => {
@@ -16,6 +17,23 @@ describe('<ds-tab-group>', () => {
       `);
 
       await expect(tabGroup).to.be.accessible();
+    });
+
+    it('should activate the first tab when active attribute is not provided', async () => {
+      const tabGroup = await fixture<DsTabGroup>(html`
+        <ds-tab-group label="test">
+          <ds-tab slot="tabs" id="tab-1" panel="panel-1">tab</ds-tab>
+          <ds-tab-panel id="panel-1" tab="tab-1"> panel </ds-tab-panel>
+
+          <ds-tab slot="tabs" id="tab-2" panel="panel-2">tab</ds-tab>
+          <ds-tab-panel id="panel-2" tab="tab-2"> panel </ds-tab-panel>
+        </ds-tab-group>
+      `);
+
+      const tabs = tabGroup.querySelectorAll<DsTab>('ds-tab');
+
+      expect(tabs[0].active).to.be.true;
+      expect(tabs[1].active).to.be.false;
     });
   });
 });
