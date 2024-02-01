ac0ef63f745bcfbf132730b24f32e0090f8739b4
Matin Mir <asmamir297@gmail.com>
2024-02-01 12:27:28 +0330

    dialog: add basic skeleton and methods

---
+++ src/components/dialog/dialog.component.ts
@@ -0,0 +1,43 @@
+import {html} from 'lit';
+import {customElement, query} from 'lit/decorators.js';
+import {BaseElement} from '../../internals/base-element';
+
+const ELEMENT_NAME = 'ds-dialog';
+
+@customElement(ELEMENT_NAME)
+export default class DsDialog extends BaseElement {
+  @query('dialog')
+  dialog: HTMLDialogElement;
+
+  /** Delegate show to the underlying dialog element */
+  show() {
+    this.dialog.showModal();
+  }
+
+  /** Delegate close to the underlying dialog element */
+  close() {
+    this.dialog.close();
+  }
+
+  render() {
+    return html`
+      <dialog part="base" class="dialog">
+        <header part="header" class="dialog__header">
+          <slot name="header"></slot>
+        </header>
+        <main part="main" class="dialog__main">
+          <slot></slot>
+        </main>
+        <footer part="footer" class="dialog__footer">
+          <slot name="footer"></slot>
+        </footer>
+      </dialog>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [ELEMENT_NAME]: DsDialog;
+  }
+}
